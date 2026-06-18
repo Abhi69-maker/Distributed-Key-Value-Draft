579396ee4b30fa9a7c16e3f2bee94676fe15ccfd
Ajay_00 <ajaylohith855@gmail.com>
2026-06-18 14:13:21 +0530

    Initial commit

---
+++ backend/replication-service/replication-service/src/main/java/com/dist/replication_service/ReplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dist.replication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicationServiceApplication.class, args);
+	}
+
+}
---
+++ backend/replication-service/replication-service/src/test/java/com/dist/replication_service/ReplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dist.replication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
