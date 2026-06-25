025f024730dcefc84ee291428019eaa4abb0324b
Ajay_00 <ajaylohith855@gmail.com>
2026-06-25 11:58:58 +0530

    completed replication-service

--- backend/replication-service/replication-service/src/main/java/com/dist/replication_service/event/KvEvent.java
+++ backend/replication-service/replication-service/src/main/java/com/dist/replication_service/event/KvEvent.java
@@ -1,4 +1,18 @@
 package com.dist.replication_service.event;
 
-public class KvEvent {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record KvEvent(
+        UUID eventId,
+        EventOperation operation,
+        String key,
+        String value,
+        Long version,
+        String nodeId,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
+        LocalDateTime timestamp
+) {
+    public enum EventOperation { PUT, UPDATE, DELETE }
 }
